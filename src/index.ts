726a77bae0ef44ab77c42c68fede6dc590d61f25
admin_haru <moto55.haru13@gmail.com>
2020-12-02 22:06:57 +0900

    @vue/web-component-wrapper

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,11 @@ import Vue from "vue";
 import vueCustomElement from "vue-custom-element";
 import CustomButton from "./custom-button.vue";
 import CustomTextField from "./custom-text-field.vue";
+import wrap from "@vue/web-component-wrapper";
 
-Vue.config.ignoredElements = ["custom-button", "custom-text-field"];
+Vue.config.ignoredElements = ["custom-text-field"];
 Vue.use(vueCustomElement);
-Vue.customElement("custom-button", CustomButton, {
-  // shadowDOM化すると中のCSSが効かず、ここで指定したCSSが効く
-  shadow: true,
-  shadowCss: `
-      button {
-      font-size: 8px;
-      cursor: pointer;
-      color: magenta;
-      padding: 1em 1em;
-      background: PaleGreen;
-    `,
-});
 Vue.customElement("custom-text-field", CustomTextField);
+
+const CustomButtonElement = wrap(Vue, CustomButton);
+window.customElements.define("custom-button", CustomButtonElement);
